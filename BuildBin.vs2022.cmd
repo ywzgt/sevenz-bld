b9fc47950231521b188d2ee1423641c8c4964091
ywzgt <ywzgt@users.noreply.github.com>
2024-04-23 16:05:36 +0800

    init

---
+++ BuildBin.vs2022.cmd
@@ -0,0 +1,55 @@
+@echo off
+rem https://github.com/WinMerge/sevenzip/raw/master/BuildBin.vs2022.cmd
+
+cd /d "%~dp0"
+
+for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\microsoft visual studio\installer\vswhere.exe" -version [17.0^,18.0^) -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+if "%1" == "" (
+  call :BuildBin x86 || goto :eof
+  call :BuildBin x64 || goto :eof
+  call :BuildBin ARM || goto :eof
+  call :BuildBin ARM64 || goto :eof
+) else (
+  call :BuildBin %1 || goto :eof
+)
+
+goto :eof
+
+:BuildBin
+
+setlocal
+
+if "%1" == "Win32" set ARCH=x86
+if "%1" == "x86" set ARCH=x86
+if "%1" == "X86" set ARCH=x86
+if "%1" == "x64" set ARCH=amd64
+if "%1" == "X64" set ARCH=amd64
+if "%1" == "arm" set ARCH=amd64_arm
+if "%1" == "ARM" set ARCH=amd64_arm
+if "%1" == "arm64" set ARCH=amd64_arm64
+if "%1" == "ARM64" set ARCH=amd64_arm64
+
+call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
+
+pushd 7z\CPP\7zip
+echo ::
+echo ::Building 7zip for %1
+echo ::
+set CL=/MP
+set CFLAGS=%CFLAGS% %CL%
+set CXXFLAGS=%CXXFLAGS% %CL%
+nmake
+popd
+
+if exist "%SIGNBAT_PATH%" (
+  if "%ARCH%" == "x86" (
+    call "%SIGNBAT_PATH%" CPP\7zip\Bundles\Format7zF\x86\7z.dll
+  ) else (
+    call "%SIGNBAT_PATH%" CPP\7zip\Bundles\Format7zF\%1\7z.dll
+  )
+)
+
+endlocal
+goto :eof
---
+++ build.sh
@@ -0,0 +1,30 @@
+set -ex
+
+git clone https://github.com/ip7z/7zip 7z
+
+# icons
+cp -f icons/FM/*.bmp 7z/CPP/7zip/UI/FileManager
+cp -f icons/Archive/*.ico 7z/CPP/7zip/Archive/Icons
+
+version=$(grep '^#define MY_VERSION_NUMBERS' 7z/C/7zVersion.h | cut -d' ' -f3 | sed 's/\s\|"//g')
+echo "version=${version}" >> $GITHUB_ENV
+
+./BuildBin.vs2022.cmd
+
+while read -r f; do
+	arch=${f%/*}
+	arch=${arch##*/}
+	install -Dvt ./${arch} "$f"
+done < <(find 7z -name \*.sfx -o -name \*.exe -o -name \*.dll)
+
+wget -nv https://7-zip.org/a/7z${version//.}-x64.exe
+x64/7z.exe x 7z${version//.}-x64.exe
+
+for arch in x86 x64 arm arm64; do
+	(cd ${arch}
+	[[ -z $(find ../7z -name \*.txt) ]] || find ../7z -name \*.txt | xargs install -vt .
+	[ ! -f src-history.txt ] || mv {src-,}history.txt
+	cp -r ../Lang .
+	cp -r ../7-zip.chm . || true
+	../x64/7zr.exe a ../7zip-${version}-${arch}.7z .)
+done
